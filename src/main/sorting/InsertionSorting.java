f88c52a0322491c01d8f134176bf8460767da6a3
yuhyeonct <yuhyeon.lee@cointelli.com>
2022-04-07 00:50:59 +0900

    refactor: insertionSorting

--- src/main/sorting/InsertionSorting.java
+++ src/main/sorting/InsertionSorting.java
@@ -10,24 +10,34 @@ public class InsertionSorting {
         Scanner in = new Scanner(System.in);
         int totalNumber = in.nextInt();
         int[] numbers = new int[totalNumber];
-        for(int i = 0; i < totalNumber; i++) {
+        for (int i = 0; i < totalNumber; i++) {
             numbers[i] = in.nextInt();
         }
 
         for (int i = 1; i < totalNumber; i++) {
-            int idx = i;
-            for (int j = i - 1; j >= 0; j--) {
-                if (numbers[idx] < numbers[j]) {
-                    int tmp = numbers[idx];
-                    numbers[idx] = numbers[j];
-                    numbers[j] = tmp;
-                    idx--;
-                } else {
-                    break;
-                }
+            int key = numbers[i];
+            /* while문으로 리팩토링 */
+//            for (int j = i - 1; j >= 0; j--) {
+//                if (key < numbers[j]) {
+//                    numbers[j + 1] = numbers[j];
+//                } else {
+//                    numbers[j + 1] = key;
+//                    break;
+//                }
+//                if (j == 0) {
+//                    numbers[j] = key;
+//                }
+//            }
+            int j = i - 1;
+            while (j >= 0 && numbers[j] > key) {
+                numbers[j + 1] = numbers[j];
+                j--;
             }
+            numbers[j + 1] = key;
         }
 
-        Arrays.stream(numbers).forEach(System.out::println);
+        Arrays.stream(numbers).forEach(e -> {
+            System.out.print(e + " ");
+        });
     }
 }

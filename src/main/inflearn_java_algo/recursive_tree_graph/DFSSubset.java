c312fd6ad00c89a104d8c2742e0077ef1594588a
baekho <klyhyeon@gmail.com>
2022-10-12 23:54:16 +0900

    feat: 부분집합 DFS 코드 작성중

---
+++ src/main/inflearn_java_algo/recursive_tree_graph/DFSSubset.java
@@ -0,0 +1,30 @@
+package inflearn_java_algo.recursive_tree_graph;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class DFSSubset {
+  //자연수 N이 주어지면 1부터 N까지의 원소를 갖는 집합의 부분집합을 모두 출력하는 프로그램
+  //입력: 자연수 N이 주어집니다. (1<=N<=10)
+  //출력: 첫 번째 줄부터 부분집합을 작은 숫자원소부터 차례대로 출력
+
+  private static int[] ch;
+  private static int inputNum;
+
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(new InputStreamReader(System.in));
+    inputNum = sc.nextInt();
+    subset(1);
+  }
+
+  private static void subset(int num) {
+     if (num == inputNum + 1) {
+       return;
+     }
+     ch[num] = 1;
+     subset(num + 1);
+     ch[num] = 0;
+     subset(num + 1);
+  }
+}

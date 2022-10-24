60d6530bfc1fb6526546e99e01e3fe164c455a34
baekho <klyhyeon@gmail.com>
2022-10-25 00:56:49 +0900

    feat: 송아지 찾기 코드 작성중

--- src/main/inflearn_java_algo/recursive_tree_graph/FindingCalf.java
+++ src/main/inflearn_java_algo/recursive_tree_graph/FindingCalf.java
@@ -1,19 +1,57 @@
 package inflearn_java_algo.recursive_tree_graph;
 
-public class FindingCalf {
-    public static void main(String[] args){
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
 
+
+public class FindingCalf {
+
+    static boolean[] checkPoints = new boolean[10001];
+
+    static int calfStatic = 0;
+
+    static Queue<Integer> checkPointBox = new LinkedList<>();
+
+    static int answer = 0;
+
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int input1 = in.nextInt();
+        int input2 = in.nextInt();
+        System.out.println(solution(input1, input2));
     }
 
-    //TODO 코드 완성
-    public void solution(int hs, int calf) {
-        int goal = calf - hs;
-        int min = 0;
-        while (goal == 0) {
-            if (goal > 5) {
-                min += 1;
-                goal -= 5;
+    public static int solution(int hs, int calf) {
+        calfStatic = calf;
+
+        checkPointBox.offer(hs);
+        while (!checkPointBox.isEmpty()) {
+            answer++;
+            int checkPointBoxSize = checkPointBox.size();
+            for (int i = 0; i < checkPointBoxSize; i++) {
+                hs = checkPointBox.poll();
+                if (BFS(hs, 1) || BFS(hs, -1) || BFS(hs, 5)) {
+                    return answer;
+                }
+                checkPointBox.offer(hs + 1);
+                checkPointBox.offer(hs - 1);
+                checkPointBox.offer(hs + 5);
             }
         }
+        return answer;
+    }
+
+    private static boolean BFS(int hs, int i) {
+        if (hs + i < 0 || checkPoints[hs + i]) {
+            return false;
+        }
+
+        if (hs + i == calfStatic) {
+            return true;
+        }
+        checkPoints[hs + i] = true;
+        return false;
     }
 }

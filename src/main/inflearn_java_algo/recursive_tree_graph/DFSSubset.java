b4b391bebcb80354e759a9dce25cb9ca7b56ad58
baekho <klyhyeon@gmail.com>
2022-11-06 22:26:51 +0900

    fix: DFS 부분집합 틀린 코드 수정

--- src/main/inflearn_java_algo/recursive_tree_graph/DFSSubset.java
+++ src/main/inflearn_java_algo/recursive_tree_graph/DFSSubset.java
@@ -1,6 +1,5 @@
 package inflearn_java_algo.recursive_tree_graph;
 
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Scanner;
 
@@ -10,28 +9,28 @@ public class DFSSubset {
   //출력: 첫 번째 줄부터 부분집합을 작은 숫자원소부터 차례대로 출력
 
   private static int[] ch;
-  private static int inputNum;
+  private static int maxNum;
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(new InputStreamReader(System.in));
-    inputNum = sc.nextInt();
-    ch = new int[inputNum + 1];
+    maxNum = sc.nextInt();
+    ch = new int[maxNum + 1];
     subset(1);
   }
 
   private static void subset(int num) {
-     if (num == inputNum + 1) {
-       for (int i = 1; i <= inputNum; i++) {
-         if (ch[i] == 1) {
-           System.out.print(i + " ");
-         }
-       }
-       System.out.println();
-       return;
-     }
-     ch[num] = 1;
-     subset(num + 1);
-     ch[num] = 0;
-     subset(num + 1);
+    if (num == maxNum + 1) {
+      for (int i = 1; i < num; i++) {
+        if (ch[i] == 1) {
+          System.out.print(i + " ");
+        }
+      }
+      System.out.println();
+      return;
+    }
+    ch[num] = 1;
+    subset(num + 1);
+    ch[num] = 0;
+    subset(num + 1);
   }
 }

38ce8d4372a8edf80ddda91b1e06fdb6a08be29a
baekho <klyhyeon@gmail.com>
2022-12-10 14:05:20 +0900

    feat: 스택 큐 다리를 지나는 트럭 문제 완료

---
+++ src/main/programmers/stack_queue/Main.java
@@ -0,0 +1,9 @@
+package programmers.stack_queue;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Truck truck = new Truck();
+        System.out.println(truck.solution(2, 10, new int[]{7, 4, 5, 6}));
+    }
+}

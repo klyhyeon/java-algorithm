42492cd09e64619e0e8d95719fc7f3fb2126fcc1
baekho <klyhyeon@gmail.com>
2022-12-31 13:35:29 +0900

    feat: 타겟넘버 dfs 풀이

---
+++ src/main/programmers/dfs_bfs/main/Main.java
@@ -0,0 +1,11 @@
+package programmers.dfs_bfs.main;
+
+import programmers.dfs_bfs.TargetNum;
+
+public class Main {
+
+    public static void main(String[] args) {
+        TargetNum targetNum = new TargetNum();
+        System.out.println(targetNum.solution(new int[]{1,1,1,1,1}, 3));
+    }
+}

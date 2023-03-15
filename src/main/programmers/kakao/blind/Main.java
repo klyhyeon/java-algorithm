684b197591d363f31e404e118e6b41e8197b6f51
Baekho <klyhyeon@gmail.com>
2023-03-15 09:30:37 +0900

    feat: WIP 프로그래머스 카카오 오픈채팅방 리팩토링 중

--- src/main/programmers/kakao/blind/Main.java
+++ src/main/programmers/kakao/blind/Main.java
@@ -2,10 +2,10 @@ package programmers.kakao.blind;
 
 public class Main {
 
-    public static void main(String[] args) {
-        OpenChatRoom openChatRoom = new OpenChatRoom();
-        openChatRoom.solution(
-                new String[]{"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"}
-        );
+  public static void main(String[] args) {
+    OpenChatRoom openChatRoom = new OpenChatRoom();
+    for (String s : openChatRoom.solution(new String[] {"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"})) {
+      System.out.println(s);
     }
+  }
 }
--- src/main/programmers/kakao/blind/OpenChatRoom.java
+++ src/main/programmers/kakao/blind/OpenChatRoom.java
@@ -2,32 +2,72 @@ package programmers.kakao.blind;
 
 import java.util.HashMap;
 import java.util.Iterator;
+import java.util.List;
 import java.util.Map;
 
 public class OpenChatRoom {
+  /*
+   * ["Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"]
+   * ["Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."]
+   */
 
-    public String[] solution(String[] record) {
-        String[] answer = new String[record.length];
-        Map<String, String> userInfo = new HashMap<>();
-        for (String str : record) {
-            String[] strs = str.split(" ");
-            String action = strs[0];
-            String userId = strs[1];
-            String nickName = strs[2];
-            if (action.equals("Enter")) {
-                userInfo.put(userId, nickName);
-            }
-        }
-        for (String str : record) {
-            Iterator<Map.Entry<String, String>> iterator = userInfo.entrySet().iterator();
-            while (iterator.hasNext()) {
-                Map.Entry<String, String> entrySet = iterator.next();
-                if (str.contains(entrySet.getKey())) {
-                    answer[i] = entrySet.getValue() + " ";
-                }
-            }
-        }
-        return answer;
+  /*
+   * 1. 유저 네임을 고쳐주기
+   * 2. 커맨드에 따라 문장을 한글로 수정해주기
+   * 3. UserInfo 클래스와 Command 클래스 만들기
+   */
+
+  class UserInfo {
+
+    String userName;
+
+    char action;
+
+    public UserInfo(String userName, char action) {
+      this.userName = userName;
+      this.action = action;
     }
+  }
+
+  class CommandInfo {
+
+    char command;
+
+    String userName;
+  }
+
+  public List<String> solution(String[] record) {
+    Map<String, UserInfo> userIdInfoMap = new HashMap<>();
+    String[] answer = new String[record.length];
+    for (String str : record) {
+      String[] words = str.split(" ");
+      if (str.charAt(0) == 'E') {
+        userIdInfoMap.put(words[1], new UserInfo(words[2], 'E'));
+      }
+    }
+
+    int i = 0;
+    for (String str : record) {
+      Iterator<Map.Entry<String, String>> iterator = userInfo.entrySet().iterator();
+      while (iterator.hasNext()) {
+        Map.Entry<String, String> entrySet = iterator.next();
+        if (str.contains(entrySet.getKey())) {
+          String[] words = str.split(" ");
+          String action = words[0];
+          String convertedAction;
+          if (action.equals("Enter")) {
+            convertedAction = "들어왔습니다.";
+          } else if (action.equals("Leave")) {
+            convertedAction = "나갔습니다.";
+          } else {
+            continue;
+          }
+          answer[i] = entrySet.getValue() + "님이 " + convertedAction;
+        }
+      }
+      i++;
+    }
+    return answer;
+  }
 }
 

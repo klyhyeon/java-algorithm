e8f059db4640bfe742d1fb20ef9c3bee3a6ad59c
Baekho <klyhyeon@gmail.com>
2023-05-18 22:06:56 +0900

    feat: 의상 완료

--- src/main/programmers/hash/Clothes.java
+++ src/main/programmers/hash/Clothes.java
@@ -6,25 +6,24 @@ import java.util.List;
 import java.util.Map;
 
 public class Clothes {
-    public int solution(String[][] clothes) {
-        int answer = 0;
-        Map<String, Integer> clothesKindMap = new HashMap<>();
-        for (String[] clothesSet : clothes) {
-            Integer clothesCount = clothesKindMap.get(clothesSet[1]);
-            if (clothesCount == null) {
-                clothesCount = 0;
-            }
-            clothesKindMap.put(clothesSet[0], ++clothesCount);
-        }
-        answer = clothes.length;
-        if (clothesKindMap.size() > 1) {
-            List<Integer> v = new ArrayList<>(clothesKindMap.values());
-            int multipliedNum = 1;
-            for (Integer integer : v) {
-                multipliedNum *= integer;
-            }
-            answer += multipliedNum;
-        }
-        return answer;
+  public int solution(String[][] clothes) {
+    int answer = 0;
+    Map<String, Integer> clothesKindMap = new HashMap<>();
+    for (String[] clothesSet : clothes) {
+      Integer clothesCount = clothesKindMap.get(clothesSet[1]);
+      if (clothesCount == null) {
+        clothesCount = 1;
+      }
+      clothesKindMap.put(clothesSet[1], ++clothesCount);
     }
+    if (clothesKindMap.size() > 0) {
+      List<Integer> v = new ArrayList<>(clothesKindMap.values());
+      int multipliedResult = 1;
+      for (Integer integer : v) {
+        multipliedResult *= integer;
+      }
+      answer += multipliedResult;
+    }
+    return answer - 1;
+  }
 }

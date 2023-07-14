3f7299c9ae6096553e0a694c4d4f1b94f20bc4c6
baekho <klyhyeon@gmail.com>
2023-07-14 20:49:05 +0900

    feat: Fastest Path

--- src/main/programmers/FastestPath.java
+++ src/main/programmers/FastestPath.java
@@ -1,2 +1,37 @@
-package programmers;public class FastestPath {
+package programmers;
+
+public class FastestPath {
+
+  int[] dx = {1, 0, -1, 0};
+  int[] dy = {0, 1, 0, -1};
+
+  //최단거리 맵
+  public int solution(int[][] map) {
+    int answer = 0;
+    boolean[][] visited = new boolean[map.length][map[0].length];
+    int[][] count = new int[map.length][map[0].length];
+
+    return answer;
+  }
+}
+
+//TODO Queue 만들고 객체 완성
+class Coordinate {
+
+  int x;
+
+  int y;
+
+  public Coordinate(int x, int y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  public int getX() {
+    return x;
+  }
+
+  public int getY() {
+    return y;
+  }
 }
